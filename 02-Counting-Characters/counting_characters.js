baa8a17bb86a8f466be491a99d189f25766faf72
khaled sidawi <khaledassa123@gmail.com>
2021-02-19 12:59:28 +0100

    i create my exe2

--- 02-Counting-Characters/counting_characters.js
+++ 02-Counting-Characters/counting_characters.js
@@ -1,8 +1,16 @@
 //insert your pseudocode below
 /*
-
+create a variable to store the string "text"
+and ask a user tp enter string
+create another variable to store the length 
+and then print the text that the users entered and the length
 here
 
 */
 
 //insert your code below
+var text, length_of_text; 
+text = window.prompt('abc');
+length_of_text = text.length;
+window.alert(length_of_text);
+window.alert(text);
