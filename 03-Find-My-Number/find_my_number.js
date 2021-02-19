2acbbca16aa9832c415f38fd093969b8ff9ddab1
khaled sidawi <khaledassa123@gmail.com>
2021-02-19 15:48:56 +0100

    i added exer3

--- 03-Find-My-Number/find_my_number.js
+++ 03-Find-My-Number/find_my_number.js
@@ -1,8 +1,38 @@
 //insert your pseudocode below
 /*
-
-here
-
+il computer pensa un numero da 1 a 100 
+il user deve indovinare il numero 
+mettendo il numero nella finestra 
+quando il computer mette il numero e il user mette altro numer si dice se il numero più alto o più basso o uguale 
+a il numero di computer 
+alla fine si dice si
+ 
 */
 
 //insert your code below
+var i, num;
+
+function mathRandomInt(a, b) {
+  if (a > b) {
+    // Swap a and b to ensure a is smaller.
+    var c = a;
+    a = b;
+    b = c;
+  }
+  return Math.floor(Math.random() * (b - a + 1) + a);
+}
+
+
+i = Number(window.prompt('guess the number'));
+num = mathRandomInt(1, 100);
+while (num != i) {
+  if (num > i) {
+    i = Number(window.prompt('your number is a bigger '));
+  }
+  if (num < i) {
+    i = Number(window.prompt('your number is smaller'));
+  }
+}
+window.alert('congratulations!');
+
+1 + 1;
