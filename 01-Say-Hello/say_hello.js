916830b475e6af664c47e06f9f0c0734e3d0d0c8
khaled sidawi <khaledassa123@gmail.com>
2021-02-19 12:47:25 +0100

    i  create the pro say

--- 01-Say-Hello/say_hello.js
+++ 01-Say-Hello/say_hello.js
@@ -1,8 +1,13 @@
 //insert your pseudocode below
 /*
-
-here
-
-*/
+create a variable to store  the string
+create another variable
+ ask the users to enter sting 
+ 
+*/  
 
 //insert your code below
+var greetin, name2;
+greeting = 'Hello  ';
+name2 = window.prompt('Enter your name');
+window.alert(greeting + name2);
